7510525af7b80fd5fff762499ede8761a453d364
easton-chen <eastonchen8@gmail.com>
2018-10-25 13:50:18 +0800

    add model

--- lc/attrlocalController.py
+++ lc/attrlocalController.py
@@ -13,22 +13,25 @@ from time import sleep
 import time
 import random
 import copy
+from load_model import *
+
 
 # Controller logic
-def executeController(pole, setPoint, serviceTime, serviceLevel):
+def executeController(pole, setPoint, timeoutRate, avgServiceTime, serviceLevel):
 	# special value: no control
 	if pole == 0:
 		return serviceLevel
-
-	alpha = serviceTime / serviceLevel # very rough estimate
+	
+	#alpha = serviceTime / serviceLevel # very rough estimate
 	# NOTE: control knob allowing to smooth service times
 	# To enable this, you *must* add a new state variable (alpha) to the controller.
-	#alpha = 0.5 * alpha + 0.5 * serviceTime / previousServiceLevel # very rough estimate
-	error = setPoint - serviceTime
+		
+	#alpha = 0.5 * alpha + 0.5 * maxServiceTime / serviceLevel # very rough estimate
+	#error = setPoint - maxServiceTime + 0.5*setPoint - avgServiceTime
 	# NOTE: control knob allowing slow increase
-	if error > 0:
+	#if error > 0:
 		error *= 0.1
-	serviceLevel = serviceLevel + (1 / alpha) * (1 - pole) * error
+	serviceLevel = serviceLevel * (timeoutRate + avgServiceTime - 0.7)
 
 	# saturation, service level is a probability
 	serviceLevel = max(serviceLevel, 0.0)
@@ -69,8 +72,12 @@ def quartiles(a):
 	ret.append(median(a[n//2:]))
 	ret.append(a[-1])
 	ret.append(avg(a))
-
 	return ret
+
+class UnixTimeStampFormatter(logging.Formatter):
+	def formatTime(self, record, datefmt = None):
+		return "{0:.6f}".format(record.created)
+
 
 def getNumberRequestsHigherLatency(latencies, setPoint):
 	compteur = 0
@@ -78,20 +85,29 @@ def getNumberRequestsHigherLatency(latencies, setPoint):
 		if i>setPoint:
 			compteur += 1
 	p_timeout = compteur/(len(latencies))
-	p_timeout *= 10	
+	if(p_timeout > 0.5):
+		p_timeout *= 10
+	else:
+		p_timeout *= 10	
 	return 1/(e**p_timeout)
 
 def getAverageServiceTime(latencies, setPoint):
 	p_avg = avg(latencies)/setPoint
+	'''
+	if(p_avg <= 0.4):
+		p_avg /= 5
+	elif(p_avg <= 0.6):
+		p_avg /= 2
+	else:
+		p_avg /= 1.5
+		
+	return 1/(e**p_avg)
+	'''
 	a = 0.99290
 	b = 0.08692
 	c = -1.12247 
 	res = a + b * p_avg + c * p_avg * p_avg
 	return max(0, res)
-
-class UnixTimeStampFormatter(logging.Formatter):
-	def formatTime(self, record, datefmt = None):
-		return "{0:.6f}".format(record.created)
 
 def main():
 	# Set up logging
@@ -129,24 +145,31 @@ def main():
 	lastTotalRequests = 0
 	timestampedLatencies = [] # tuples of timestamp, latency
 	totalRequests = 0
-	serviceLevel = 0.5
-    	lastServiceLevel = 0
+	serviceLevel = options.serviceLevel
 	cap = options.cap
 	concurrency = options.concurrency
 	thinktime = options.thinktime
-	setPoint = options.setPoint
-    	flag = 0
-        preference_order_list = [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]]
+	preference_order_list = [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]]
 	preference_order = preference_order_list[options.preference]
-        weights = [0, 0, 0]
-	weights[preference_order[0]] = 0.6
-        weights[preference_order[1]] = 0.3
-        weights[preference_order[2]] = 0.1
+	controlNO = 0
+	init_latency = 0
+	init_serviceLevel = 0
+
+	# external
+	X = []
+	preference_order.append(cap)
+	preference_order.append(concurrency)
+	X = np.array([preference_order])
+	Y = use_model(X, EDName)
+
+	# internal
 
 
+	
 	# Control loop
-	while True:
-        	
+	while(1):
+		if(controlNO > 23):
+			break
 		# Wait for next control iteration or message from application
 		waitFor = max(ceil((lastControl + options.controlInterval - now()) * 1000), 1)
 		events = poll.poll(waitFor)
@@ -160,6 +183,7 @@ def main():
 
 		# Run control algorithm if it's time for it
 		if _now - lastControl >= options.controlInterval:
+			
 			# Filter latencies: only take those from the measure interval
 			timestampedLatencies = [ (t, l)
 				for t, l in timestampedLatencies if t > _now - options.measureInterval ]
@@ -167,48 +191,81 @@ def main():
 
 			# Do we have new reports?
 			if latencies:
-                                # if converge        
-                                if abs(serviceLevel - lastServiceLevel) <= 0.01:
-        	    	                flag += 1
-				else:
-					flag = 0
-        	                if flag == 3:
-                                        y1 = getNumberRequestsHigherLatency(latencies, setPoint)
-                                        y2 = serviceLevel
-                                        y3 = getAverageServiceTime(latencies, setPoint)
-                                        Y = y1 * weights[0] + y2 * weights[1] + y3 * weights[2]
-                                        line = ','.join([str(options.preference), str(cap), str(concurrency),str(Y)]) + '\n'
-                                        line = 'data: ' + line
-                                        logging.info(line)
-                                        break
+				'''
 				# Execute controller
-                		lastServiceLevel = serviceLevel
 				serviceLevel = executeController(
 					pole = options.pole,
 					setPoint = options.setPoint,
 					serviceTime = max(latencies),
 					serviceLevel = serviceLevel,
 				)
+				'''
+				#added
 				
+				numberRequestsHigherLatency = getNumberRequestsHigherLatency(latencies, options.setPoint)
+				averageServiceTime = getAverageServiceTime(latencies, options.setPoint)
+				
+				bestweights, besty = idealWeights(preference_order, numberRequestsHigherLatency, serviceLevel, averageServiceTime, numberTries = 1000)
+				if(averageServiceTime == 0):
+					besty = 0
+
+				if(besty > Besty):
+					Besty = besty
+					Bestwights = copy.deepcopy(bestweights)
+					Bestrr = serviceLevel
+				
+		
+				'''
 				# Report performance to RM
 				matchingValue = min([ 1 - latency / options.setPoint for latency in latencies ])
-				rmSocket.sendto(str(matchingValue), (options.rmIp, options.rmPort))
 
+				#added new values
+				rmSocket.sendto(str(matchingValue), (options.rmIp, options.rmPort))
+				'''
 				# Print statistics
 				latencyStat = quartiles(latencies)
-				logging.info("latency={0:.0f}:{1:.0f}:{2:.0f}:{3:.0f}:{4:.0f}:({5:.0f})ms throughput={6:.0f}rps rr={7:.2f}% total={8} perf={9:.3f} conv?={10}".format(
+				if(controlNO == 3):
+					init_serviceLevel = 0
+					init_latency = latencyStat[5] * 1000
+				logging.info("Control No.{13} latency={0:.0f}:{1:.0f}:{2:.0f}:{3:.0f}:{4:.0f}:({5:.0f})ms y1={11:.2f} rr(y2)={6:.2f}% y3={12:.2f} weights={7} Y={8} cap={9} concurrency={10} thinktime={14} init_latency={15}ms init_serviceLevel={16}% preference={17}".format(
 					latencyStat[0] * 1000,
 					latencyStat[1] * 1000,
 					latencyStat[2] * 1000,
 					latencyStat[3] * 1000,
 					latencyStat[4] * 1000,
 					latencyStat[5] * 1000,
-					(totalRequests - lastTotalRequests) / (_now-lastControl),
 					serviceLevel * 100,
-					totalRequests,
-					matchingValue,
-					lastServiceLevel - serviceLevel
-				))
+					bestweights,
+					besty,
+					cap,
+					concurrency,
+					averageServiceTime,
+					numberRequestsHigherLatency,
+					controlNO,
+					thinktime,
+					init_latency,
+					init_serviceLevel * 100,
+					options.preference
+				))	
+				logging.getLogger().handlers[0].flush()	
+								
+				controlNO += 1
+				
+				# choose random service level
+				
+				if(controlNO > 3):				
+					serviceLevel = 0.05 * (controlNO - 3)
+					serviceLevel = min(1,serviceLevel)
+				'''
+				# Execute controller
+				serviceLevel = executeController(
+					pole = options.pole,
+					setPoint = options.setPoint,
+					timeoutRate = numberRequestsHigherLatency,
+					avgServiceTime = averageServiceTime,
+					serviceLevel = serviceLevel,
+				)
+				'''
 				with open('/tmp/serviceLevel.tmp', 'w') as f:
 					print(serviceLevel, file = f)
 				os.rename('/tmp/serviceLevel.tmp', '/tmp/serviceLevel')
@@ -220,3 +277,6 @@ def main():
 
 if __name__ == "__main__":
 	main()
+	logging.info("lc end")
+
+
