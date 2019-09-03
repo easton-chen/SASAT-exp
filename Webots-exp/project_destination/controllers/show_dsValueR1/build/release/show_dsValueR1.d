0b06a885a16045a9f4918eba6e913317153d6cb6
easton-chen <eastonchen8@gmail.com>
2019-09-03 15:00:37 +0800

    rm old file

--- Webots-exp/project_destination/controllers/show_dsValueR1/build/release/show_dsValueR1.d
+++
@@ -1,8 +0,0 @@
-build/release/show_dsValueR1.o: show_dsValueR1.c \
- C:\Users\lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
- C:\Users\lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
- C:\Users\lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h
--- Webots-exp/project_destination/readme.txt
+++
@@ -1,5 +0,0 @@
-There are three experiments in the folder of controllers and worlds (all the setting about the world in folder "worlds", and all the control algorithm of the car are in folder "controllers"):
-	world			controller
-1. 	color_ds_roughness1.ds	show_dsValueR1.c
-2. 	color_ds.ds		show_dsValue.c
-3. 	color_dsNOAGeneric.ds	show_dsValueNoAttributeR1.c
